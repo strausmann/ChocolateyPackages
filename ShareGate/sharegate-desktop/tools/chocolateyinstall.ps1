89326decb0aaa0682a578da7d48a018f78d0db38
Björn Strausmann <bjoern@strausmann.net>
2021-04-07 13:36:44 +0200

    Update ShareGate to 14.2.0
    #6 Closed

--- ShareGate/sharegate-desktop/tools/chocolateyinstall.ps1
+++ ShareGate/sharegate-desktop/tools/chocolateyinstall.ps1
@@ -8,11 +8,11 @@ $packageArgs = @{
   fileType      = 'MSI'
   silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`" SHAREGATEINSTALLSCOPE=PERUSER LAUNCHSHAREGATEONEXIT=1"
   validExitCodes= @(0,1641,3010)
-  url           = "https://assets.sharegate.com/sharegate/desktop/downloads/Sharegate.13.1.3.msi"
-  checksum      = 'DECB5CAEFD22B2C423FF5E6E24FF5716A2819295C1B1BBF2850A91265F9DAE93'
+  url           = "https://assets.sharegate.com/sharegate/desktop/downloads/Sharegate.14.2.0.msi"
+  checksum      = 'D338981696BD70450EF9343341F3D6ED9B496981E1F05EC976911EEF313D1B5C'
   checksumType  = 'sha256'
-  url64bit      = "https://assets.sharegate.com/sharegate/desktop/downloads/Sharegate.13.1.3.msi"
-  checksum64    = 'DECB5CAEFD22B2C423FF5E6E24FF5716A2819295C1B1BBF2850A91265F9DAE93'
+  url64bit      = "https://assets.sharegate.com/sharegate/desktop/downloads/Sharegate.14.2.0.msi"
+  checksum64    = 'D338981696BD70450EF9343341F3D6ED9B496981E1F05EC976911EEF313D1B5C'
   checksumType64= 'sha256'
   destination   = $toolsDir
 }
