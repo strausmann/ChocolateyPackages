db3a8528bc84bdde58b43f7a8205dbf67ab184b6
Björn Strausmann <bjoern@strausmann.net>
2021-06-28 15:04:43 +0200

    Update ShareGate Desktop to v15.0.1 and add dependencies

--- ShareGate/sharegate-desktop/tools/chocolateyinstall.ps1
+++ ShareGate/sharegate-desktop/tools/chocolateyinstall.ps1
@@ -1,20 +1,20 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = ''
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'ShareGate Desktop*'
   fileType      = 'MSI'
-  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`" SHAREGATEINSTALLSCOPE=PERUSER LAUNCHSHAREGATEONEXIT=1"
+  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`" SHAREGATEINSTALLSCOPE=PERUSER LAUNCHSHAREGATEONEXIT=0"
   validExitCodes= @(0,1641,3010)
-  url           = "https://assets.sharegate.com/sharegate/desktop/downloads/Sharegate.14.2.0.msi"
-  checksum      = 'D338981696BD70450EF9343341F3D6ED9B496981E1F05EC976911EEF313D1B5C'
+  url           = "https://assets.sharegate.com/sharegate/desktop/downloads/Sharegate.15.0.1.msi"
+  checksum      = '8e20192d16f0481e973400d277c5841404fb713460b27931e1b5752862e7635b'
   checksumType  = 'sha256'
-  url64bit      = "https://assets.sharegate.com/sharegate/desktop/downloads/Sharegate.14.2.0.msi"
-  checksum64    = 'D338981696BD70450EF9343341F3D6ED9B496981E1F05EC976911EEF313D1B5C'
+  url64bit      = "https://assets.sharegate.com/sharegate/desktop/downloads/Sharegate.15.0.1.msi"
+  checksum64    = '8e20192d16f0481e973400d277c5841404fb713460b27931e1b5752862e7635b'
   checksumType64= 'sha256'
   destination   = $toolsDir
 }
 
+if (Get-Is32) { 'url64bit', 'checksum64', 'checksumType64' | ForEach-Object { $packageArgs.Remove($_) } }
 Install-ChocolateyPackage @packageArgs
