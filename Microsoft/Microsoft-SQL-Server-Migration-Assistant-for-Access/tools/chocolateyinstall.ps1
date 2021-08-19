72287376535ad9bdd25e43086ce6772f6e3bf436
Björn Strausmann <bjoern@strausmann.net>
2021-08-19 10:55:52 +0200

    Update Microsoft SQL Server Migration Assistant for Access to 8.21.0

--- Microsoft/Microsoft-SQL-Server-Migration-Assistant-for-Access/tools/chocolateyinstall.ps1
+++ Microsoft/Microsoft-SQL-Server-Migration-Assistant-for-Access/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url                   = 'https://download.microsoft.com/download/D/B/D/DBDECC99-4DCA-4674-983A-CC1ABABA2B37/SSMAforAccess_8.20.0_x86.msi'
-$checksum              = 'e628872183fd7ce915ecdd5fde545ec5c257269aaa5150f501f63d2799234eb7'
-$urlx64                = 'https://download.microsoft.com/download/D/B/D/DBDECC99-4DCA-4674-983A-CC1ABABA2B37/SSMAforAccess_8.20.0.msi'
-$checksumx64           = '88ccb038e17c9c328ae5590cc3447078eb224bcc04c7e4de36ea28b8521b0bdb'
+$url                   = 'https://download.microsoft.com/download/D/B/D/DBDECC99-4DCA-4674-983A-CC1ABABA2B37/SSMAforAccess_8.21.0_x86.msi'
+$checksum              = 'ae9d6f1afd2365902c32d957d82ea5f78cf8b1ae42fb63a2b6780a91af3047a4'
+$urlx64                = 'https://download.microsoft.com/download/D/B/D/DBDECC99-4DCA-4674-983A-CC1ABABA2B37/SSMAforAccess_8.21.0.msi'
+$checksumx64           = 'b07cf203495080549d7b59c8b7b9ead0b50d2b85dfd320fec6b0a9e52283fe42'
 
 $packageArgs = @{
 	packageName    = $env:ChocolateyPackageName
@@ -19,5 +19,4 @@ $packageArgs = @{
 	ChecksumType64 = 'sha256'
 }
 
-if (Get-Is32) { 'url64bit', 'checksum64', 'checksumType64' | ForEach-Object { $packageArgs.Remove($_) } }
 Install-ChocolateyPackage @packageArgs
