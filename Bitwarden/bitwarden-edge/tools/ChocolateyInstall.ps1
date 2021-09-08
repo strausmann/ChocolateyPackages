0de588f5abbb7ba0b073bdf7b382af260262918d
Björn Strausmann <bjoern@strausmann.net>
2021-09-08 22:16:44 +0200

    Fix PowerShell Syntax Issue

--- Bitwarden/bitwarden-edge/tools/ChocolateyInstall.ps1
+++ Bitwarden/bitwarden-edge/tools/ChocolateyInstall.ps1
@@ -1,11 +1,9 @@
-$extensionID = 'jbkfoedolllekgbhcbcoahefnbanhhlh'
-$valueName = '318851'
-if (Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" -Name $valueName -ErrorAction SilentlyContinue)
-{
-    Write-Host "Extension already installed." -foreground "magenta" –backgroundcolor "blue"
-}
-else
-{
+$extensionID = "jbkfoedolllekgbhcbcoahefnbanhhlh"
+$valueName = "318851"
+if (Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" -Name $valueName -ErrorAction SilentlyContinue){
+    Write-Host "Extension already installed." -foreground "magenta"
+} else {
     New-Item -Force -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" | out-null
-    New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" -Name $valueName -Value $extensionID | out-null
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" -Name $valueName -Value $extensionID -Force | out-null
+	Write-Host "Extension successfully installed." -foreground "magenta"
 }
--- Bitwarden/bitwarden-edge/tools/ChocolateyUninstall.ps1
+++ Bitwarden/bitwarden-edge/tools/ChocolateyUninstall.ps1
@@ -2,3 +2,4 @@ $extensionID = 'jbkfoedolllekgbhcbcoahefnbanhhlh'
 $valueName = '318851'
 
 Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" -Name $valueName -Force -ErrorAction SilentlyContinue | out-null
+Write-Host "Extension successfully uninstalled." -foreground "magenta"
