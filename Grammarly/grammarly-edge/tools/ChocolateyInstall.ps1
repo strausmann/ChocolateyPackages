6b69c87ddb19765b23f4e03eb1e3c7070e6a46ae
Björn Strausmann <bjoern@strausmann.net>
2021-09-09 08:59:39 +0200

    Fix PowerShell Syntax

--- Grammarly/grammarly-edge/tools/ChocolateyInstall.ps1
+++ Grammarly/grammarly-edge/tools/ChocolateyInstall.ps1
@@ -1,11 +1,9 @@
 $extensionID = 'cnlefmmeadmemmdciolhbnfeacpdfbkd'
 $valueName = '31885'
-if (Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" -Name $valueName -ErrorAction SilentlyContinue)
-{
-    Write-Host "Extension already installed." -foreground "magenta" –backgroundcolor "blue"
-}
-else
-{
+if (Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" -Name $valueName -ErrorAction SilentlyContinue){
+    Write-Host "Extension already installed." -foreground "magenta"
+} else {
     New-Item -Force -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" | out-null
-    New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" -Name $valueName -Value $extensionID | out-null
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" -Name $valueName -Value $extensionID -Force | out-null
+	Write-Host "Extension successfully installed." -foreground "magenta"
 }
--- Grammarly/grammarly-edge/tools/ChocolateyUninstall.ps1
+++ Grammarly/grammarly-edge/tools/ChocolateyUninstall.ps1
@@ -2,3 +2,4 @@ $extensionID = 'cnlefmmeadmemmdciolhbnfeacpdfbkd'
 $valueName = '31885'
 
 Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallForcelist" -Name $valueName -Force -ErrorAction SilentlyContinue | out-null
+Write-Host "Extension successfully uninstalled." -foreground "magenta"
