4707b2a05260d028676fa4120f9b9abd965e8b30
Bjoern Strausmann <bjoern.strausmann@zea-ship.com>
2021-11-17 12:29:59 +0100

    Add Logi-Tune Package

---
+++ manual/Logitech/logi-tune/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+#Based on Custom
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Logi Tune*'
+  file          = ''
+  fileType      = 'exe'
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+  url           = ""
+  checksum      = ""
+  checksumType  = ""
+  url64bit      = "https://software.vc.logitech.com/downloads/tune/LogiTuneInstall.exe"
+  checksum64    = '831D6A69BFD6195EA840F43D513646C71D9496CD1A1E983ED56D3E47CA8F37FE'
+  checksumType64= 'sha256'
+  destination   = $toolsDir
+}
+
+Install-ChocolateyInstallPackage @packageArgs
