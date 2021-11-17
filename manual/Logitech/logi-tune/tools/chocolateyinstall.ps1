2102e371225fa47f26ea603d7b96bb059eab47de
Bjoern Strausmann <bjoern.strausmann@zea-ship.com>
2021-11-17 15:00:43 +0100

    Add Logi-Tune Package

--- manual/Logitech/logi-tune/tools/chocolateyinstall.ps1
+++ manual/Logitech/logi-tune/tools/chocolateyinstall.ps1
@@ -1,21 +1,24 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir     		   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url                   = 'https://software.vc.logitech.com/downloads/tune/LogiTuneInstall.exe'
+$checksum              = '831D6A69BFD6195EA840F43D513646C71D9496CD1A1E983ED56D3E47CA8F37FE'
+$urlx64                = 'https://software.vc.logitech.com/downloads/tune/LogiTuneInstall.exe'
+$checksumx64           = '831D6A69BFD6195EA840F43D513646C71D9496CD1A1E983ED56D3E47CA8F37FE'
 
 #Based on Custom
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'Logi Tune*'
-  file          = ''
   fileType      = 'exe'
   silentArgs    = "/S"
   validExitCodes= @(0)
-  url           = ""
-  checksum      = ""
-  checksumType  = ""
-  url64bit      = "https://software.vc.logitech.com/downloads/tune/LogiTuneInstall.exe"
-  checksum64    = '831D6A69BFD6195EA840F43D513646C71D9496CD1A1E983ED56D3E47CA8F37FE'
-  checksumType64= 'sha256'
+  url           = $url
+  checksum      = $checksum
+  checksumType  = "sha256"
+  url64bit      = $urlx64
+  checksum64    = $checksumx64
+  checksumType64= "sha256"
   destination   = $toolsDir
 }
 
-Install-ChocolateyInstallPackage @packageArgs
+Install-ChocolateyPackage @packageArgs
