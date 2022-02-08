bbdb9d1c33c3ac4588a14e7ba02674b8519e3039
Bjoern Strausmann <bjoern.strausmann@zea-ship.com>
2022-02-08 13:56:25 +0100

    Add DellBIOSProvider

---
+++ manual/Dell/dellbiosprovider/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿$ErrorActionPreference = 'Stop'
+$moduleName = $env:ChocolateyPackageName  # this may be different from the package name and different case
+
+Get-InstalledModule -Name $moduleName | Uninstall-Module -AllVersions -Force -ErrorAction SilentlyContinue
