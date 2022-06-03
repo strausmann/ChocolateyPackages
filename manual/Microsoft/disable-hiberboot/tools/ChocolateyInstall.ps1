c66116d38730314621f3add203a652a8e0f2d6a1
Bjoern Strausmann <bjoern.strausmann@zea-ship.com>
2022-06-03 13:58:45 +0200

    Upgrade 1.0.1 Hiberboot Release

--- manual/Microsoft/disable-hiberboot/tools/ChocolateyInstall.ps1
+++ manual/Microsoft/disable-hiberboot/tools/ChocolateyInstall.ps1
@@ -1,7 +1,8 @@
-$valueName = "HiberbootEnabled"
-if ((Get-ItemPropertyValue -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -Name $valueName -ErrorAction SilentlyContinue) -match "0"){
-    Write-Host "HiberbootEnabled are disabled." -foreground "magenta"
-} else {
-    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -Name $valueName -Value 0 -Force | out-null
-	Write-Host "HiberbootEnabled successfully disabled." -foreground "magenta"
+if (Get-IsWinWorkstation) {
+	if (((Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -ErrorAction SilentlyContinue).HiberbootEnabled) -match "0"){
+		Write-Host "HiberbootEnabled are disabled." -ForegroundColor "Green"
+	} else {
+		Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -Name $valueName -Value 0 -Force | out-null
+		Write-Host "HiberbootEnabled successfully disabled." -ForegroundColor "Magenta"
+	}
 }
