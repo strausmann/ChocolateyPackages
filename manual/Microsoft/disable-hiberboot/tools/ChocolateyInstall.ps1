259d9662da7cd9615224fe4d5b624a6c1f86bda9
Bjoern Strausmann <bjoern.strausmann@zea-ship.com>
2022-06-13 16:02:44 +0200

    Fix Hiberboot Issue

--- manual/Microsoft/disable-hiberboot/tools/ChocolateyInstall.ps1
+++ manual/Microsoft/disable-hiberboot/tools/ChocolateyInstall.ps1
@@ -2,7 +2,9 @@ if (Get-IsWinWorkstation) {
 	if (((Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -ErrorAction SilentlyContinue).HiberbootEnabled) -match "0"){
 		Write-Host "HiberbootEnabled are disabled." -ForegroundColor "Green"
 	} else {
-		Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -Name $valueName -Value 0 -Force | out-null
+		Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -Name "HiberbootEnabled" -Value 0 -Force | out-null
 		Write-Host "HiberbootEnabled successfully disabled." -ForegroundColor "Magenta"
 	}
+} else {
+	Write-Host "The use on a Windows Server is not possible." -ForegroundColor "Magenta" 
 }
