d23120779ee01b2c9e605b53aaf5bf4b57e4f401
Bjoern Strausmann <bjoern.strausmann@zea-ship.com>
2022-06-13 16:01:46 +0200

    Add Dell SupportAssist

---
+++ manual/Dell/dellsupportassist/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir     		   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url                   = 'https://downloads.dell.com/serviceability/catalog/SupportAssistInstaller.exe'
+
+#Based on Custom
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Dell SupportAssist'
+  fileType      = 'exe'
+  silentArgs    = "/S" 
+  validExitCodes= @(0)
+  url           = $url
+  checksum      = ''
+  checksumType  = 'sha256'
+  url64bit      = ""
+  checksum64    = ''
+  checksumType64= 'sha256'
+  destination   = $toolsDir
+}
+
+Install-ChocolateyPackage @packageArgs
