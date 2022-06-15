3f596d7e8573e4b954ae598a89ee2a104a9b3ce5
Bjoern Strausmann <bjoern.strausmann@zea-ship.com>
2022-06-15 16:00:53 +0200

    Upgrade SnagIt 2020 to v2020.1.8

--- manual/TechSmith/snagit-2020/tools/chocolateyinstall.ps1
+++ manual/TechSmith/snagit-2020/tools/chocolateyinstall.ps1
@@ -2,21 +2,21 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 # Old Techsmith software versions can be found at https://www.techsmith.com/download/oldversions
-$url64 = 'https://download.techsmith.com/snagit/releases/2017/snagit.msi'
+$url64 = 'https://download.techsmith.com/snagit/releases/2018/snagit.msi'
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
 	softwareName   = 'Snagit 2020'
     installerType  = 'msi'
     url64bit       = $url64
-    checksum64     = '05b28cc613dd8d235ab7ce6e1499ca0f5de561dba5e5142837dd32c23072beda'
+    checksum64     = '3159669D04F6229EE4399EC607771FD48AEFF0DBB9BE66A806DEFBB3E9ED8209'
     checksumType64 = 'SHA256'
     silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
     validExitCodes = @(0, 3010, 1641)
 }
 
 $arguments = Get-PackageParameters -Parameter $env:chocolateyPackageParameters
-# use licensekey instead of licenseCode as this is consistent with Camtasia
+# use licensekey instead of licenseCode as this is consistent with Snagit
 if ($arguments.ContainsKey('licenseCode')) {
     if (-not ($arguments.ContainsKey('licensekey'))) {
         # create licensekey key with licenseCode value
