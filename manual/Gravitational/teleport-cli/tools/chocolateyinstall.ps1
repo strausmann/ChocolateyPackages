390fd9541c71b2ffc35b8a401eba981106cb87e3
Strausmann, Björn <bjoern.strausmann@zea-ship.com>
2022-11-04 12:16:28 +0100

    Add Teleport CLI (tsh) v11.0.1

---
+++ manual/Gravitational/teleport-cli/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+#Based on Custom
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'teleport-cli*'
+  fileType      = 'zip'
+  silentArgs    = '' 
+  validExitCodes= @(0)
+  url           = ''
+  checksum      = ''
+  checksumType  = 'sha256'
+  url64bit      = "https://cdn.teleport.dev/teleport-v11.0.1-windows-amd64-bin.zip"
+  checksum64    = '9930FDD2D08717C46E38FC2F0E76ABEDDF02CE7F1980831124DAAB160CD741BC'
+  checksumType64= 'sha256'
+  destination   = $toolsDir
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+#Get-ChocolateyUnzipCmdlet @packageArgs
+
