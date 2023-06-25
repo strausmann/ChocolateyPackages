2e295b416f58b60beb9cc29c6b56d76e7090c762
Björn Strausmann <bjoern@strausmann.net>
2023-06-25 13:53:07 +0200

    Add Bambu Studio Automatic Update

---
+++ automatic/New-Package.ps1
@@ -0,0 +1,66 @@
+param($Name, $Type)
+
+<#
+.SYNOPSIS
+    Create a new package from the template
+
+.DESCRIPTION
+    This function creates a new package by using the directory _template which contains desired package basic settings.
+#>
+function New-Package{
+    [CmdletBinding()]
+    param(
+        #Package name
+        [string] $Name,
+
+        #Type of the package
+        [ValidateSet('Portable', 'Installer', 'EPortable', 'EInstaller')]
+        [string] $Type,
+
+        #Github repository in the form username/repository
+        [string] $GithubRepository
+    )
+
+    if ($Name -eq $null) { throw "Name can't be empty" }
+    if (Test-Path $Name) { throw "Package with that name already exists" }
+    if (!(Test-Path _template)) { throw "Template for the packages not found" }
+    cp _template $Name -Recurse
+
+    $nuspec = gc "$Name\template.nuspec"
+    rm "$Name\template.nuspec"
+
+    Write-Verbose 'Fixing nuspec'
+    $nuspec = $nuspec -replace '<id>.+',               "<id>$Name</id>"
+    $nuspec = $nuspec -replace '<iconUrl>.+',          "<iconUrl>https://cdn.rawgit.com/$GithubRepository/master/automatic/$Name/icon.png</iconUrl>"
+    $nuspec = $nuspec -replace '<packageSourceUrl>.+', "<packageSourceUrl>https://github.com/$GithubRepository/tree/master/automatic/$Name</packageSourceUrl>"
+    $nuspec | Out-File -Encoding UTF8 "$Name\$Name.nuspec"
+
+    switch ($Type)
+    {
+        'Installer' {
+            Write-Verbose 'Using installer template'
+            mv "$Name\tools\chocolateyInstallExe.ps1" "$Name\tools\chocolateyInstall.ps1"
+        }
+        'Portable' {
+            Write-Verbose 'Using portable template'
+            mv "$Name\tools\chocolateyInstallZip.ps1" "$Name\tools\chocolateyInstall.ps1"
+        }
+        'EInstaller' {
+            Write-Verbose 'Using embedded installer template'
+            mv "$Name\tools\chocolateyInstallEmbeddedExe.ps1" "$Name\tools\chocolateyInstall.ps1"
+        }
+        'EPortable' {
+            Write-Verbose 'Using embedded portable template'
+            mv "$Name\tools\chocolateyInstallEmbeddedZip.ps1" "$Name\tools\chocolateyInstall.ps1"
+        }
+
+        default { throw 'No template given' }
+    }
+    rm "$Name\tools\*.ps1" -Exclude chocolateyInstall.ps1, chocolateyUninstall.ps1
+
+    Write-Verbose 'Fixing chocolateyInstall.ps1'
+    $installer = gc "$Name\tools\chocolateyInstall.ps1"
+    $installer -replace "(^[$]packageName\s*=\s*)('.*')", "`$1'$($Name)'" | Set-Content "$Name\tools\chocolateyInstall.ps1"
+}
+
+New-Package $Name $Type -GithubRepository strausmann/ChocolateyPackages -Verbose
