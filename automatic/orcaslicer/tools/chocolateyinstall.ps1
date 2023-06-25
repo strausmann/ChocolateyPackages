c7c4b60471d7f7ae63d2e2f03d9542f6f5b793e6
Björn Strausmann <bjoern@strausmann.net>
2023-06-25 17:39:32 +0200

    Update Orca Slicer AU Config

--- automatic/orcaslicer/tools/chocolateyinstall.ps1
+++ automatic/orcaslicer/tools/chocolateyinstall.ps1
@@ -6,21 +6,26 @@ $programs = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\"
 $shortcutFilePath = Join-Path $programs $linkName
 $targetPath   = Join-Path $toolsDir $fileName
 
+$url                   = 'https://github.com/SoftFever/OrcaSlicer/releases/download/v1.6.3/OrcaSlicer_V1.6.3_Win64.zip'
+$checksum              = ''
+$ChecksumType          = 'sha256'
+$url64                 = 'https://github.com/SoftFever/OrcaSlicer/releases/download/v1.6.3/OrcaSlicer_V1.6.3_Win64.zip'
+$checksum64            = ''
+$ChecksumType64        = 'sha256'
+
 #Based on Custom
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'orcaslicer*'
-  file          = ""
-  fileType      = 'zip'
-  silentArgs    = ""
-  validExitCodes= @(0)
-  url           = "https://github.com/SoftFever/OrcaSlicer/releases/download/v1.6.3/OrcaSlicer_V1.6.3_Win64.zip"
-  checksum      = '39A69BD16591BE80F4F77E01F0FED8596C6466D96ED560E1A7236CACFE7B6B8E'
-  checksumType  = 'sha256'
-  url64bit      = "$url"
-  checksum64    = '$checksum'
-  checksumType64= 'sha256'
-  destination   = $toolsDir
+  softwareName          = 'orcaslicer*'
+  fileType              = 'zip'
+  packageName           = $env:ChocolateyPackageName
+  fileFullPath          = "$toolsDir\OrcaSlicer.zip"
+  url                   = $url
+  url64                 = $url64
+  checksum              = $checksum
+  checksum64            = $checksum64
+  checksumType          = $checksumType
+  checksumType64        = $checksumType64
+  destination           = $toolsDir
 }
 
 Install-ChocolateyZipPackage @packageArgs
