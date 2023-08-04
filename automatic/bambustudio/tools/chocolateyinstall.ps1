9c31a39d8c6de55ebc601bc18c5a93fc4c4c6164
Bjoern Strausmann <bjoern@strausmann.net>
2023-08-04 16:06:40 +0000

    AU: 2 updated - bambustudio orcaslicer [skip ci] https://gist.github.com/strausmann/33cff89acf29181df2e5c7d2a3792d47/6f58871d6c085a0c73d490d88c493b87c07628fe

--- automatic/bambustudio/tools/chocolateyinstall.ps1
+++ automatic/bambustudio/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = 'https://github.com/bambulab/BambuStudio/releases/download/v01.07.01.62/Bambu_Studio_win_public-v01.07.01.62-20230801212357.exe'
-$checksum              = '283404cb716f93244c279f2e335f9bd81bee3f951a437bf87cdd9cdb4f8d4524'
+$url                   = 'https://github.com/bambulab/BambuStudio/releases/download/v01.07.02.51/Bambu_Studio_win_public-v01.07.02.51-20230804223010.exe'
+$checksum              = '95d9500d7a6768c8e7f18b41ab7fc8844ee42ee5266b8a28707c3aab17467320'
 $ChecksumType          = 'sha256'
-$url64                 = 'https://github.com/bambulab/BambuStudio/releases/download/v01.07.01.62/Bambu_Studio_win_public-v01.07.01.62-20230801212357.exe'
-$checksum64            = '283404cb716f93244c279f2e335f9bd81bee3f951a437bf87cdd9cdb4f8d4524'
+$url64                 = 'https://github.com/bambulab/BambuStudio/releases/download/v01.07.02.51/Bambu_Studio_win_public-v01.07.02.51-20230804223010.exe'
+$checksum64            = '95d9500d7a6768c8e7f18b41ab7fc8844ee42ee5266b8a28707c3aab17467320'
 $ChecksumType64        = 'sha256'
 
 $packageArgs = @{
--- automatic/orcaslicer/tools/chocolateyinstall.ps1
+++ automatic/orcaslicer/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ $programs = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\"
 $shortcutFilePath = Join-Path $programs $linkName
 $targetPath   = Join-Path $toolsDir $fileName
 
-$url                   = 'https://github.com/SoftFever/OrcaSlicer/releases/download/v1.6.4-beta/OrcaSlicer_V1.6.4-beta_Win64.zip'
-$checksum              = 'dd49e47e36d7802b0a2a973394e275226d68eef7e9a70a0eab2300119e5436ef'
+$url                   = 'https://github.com/SoftFever/OrcaSlicer/releases/download/v1.6.4-beta2/OrcaSlicer_V1.6.4-beta2_Win64.zip'
+$checksum              = 'd0eb3e476442e6d9c7ae34c6e7dbf9d477251543fd30c35ca2605b16de78e74f'
 $ChecksumType          = 'sha256'
-$url64                 = 'https://github.com/SoftFever/OrcaSlicer/releases/download/v1.6.4-beta/OrcaSlicer_V1.6.4-beta_Win64.zip'
-$checksum64            = 'dd49e47e36d7802b0a2a973394e275226d68eef7e9a70a0eab2300119e5436ef'
+$url64                 = 'https://github.com/SoftFever/OrcaSlicer/releases/download/v1.6.4-beta2/OrcaSlicer_V1.6.4-beta2_Win64.zip'
+$checksum64            = 'd0eb3e476442e6d9c7ae34c6e7dbf9d477251543fd30c35ca2605b16de78e74f'
 $ChecksumType64        = 'sha256'
 
 #Based on Custom
