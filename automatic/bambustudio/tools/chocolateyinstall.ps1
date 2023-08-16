1e2cf286aafd5d11c13c2d6208fa258b018df728
Bjoern Strausmann <bjoern.strausmann@zea-ship.com>
2023-08-16 17:11:09 +0200

    Fix Bambu Studio to Install-ChocolateyPackage

--- automatic/bambustudio/tools/chocolateyinstall.ps1
+++ automatic/bambustudio/tools/chocolateyinstall.ps1
@@ -1,6 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'BambuStudio.exe'
 
 $url                   = 'https://github.com/bambulab/BambuStudio/releases/download/v01.07.04.52/Bambu_Studio_win_public-v01.07.04.52-20230816204101.exe'
 $checksum              = '65dfcd1f64cfad101f8e2a54585314ad1414005a7f41c05f25e6c8aa9063a85b'
@@ -11,7 +12,11 @@ $ChecksumType64        = 'sha256'
 
 $packageArgs = @{
   packageName           = $env:ChocolateyPackageName
-  fileFullPath          = "$toolsDir\BambuStudio.exe"
+  softwareName          = 'BambuStudio*'
+  file                  = $fileLocation
+  fileType              = 'exe'
+  silentArgs            = "/S"
+  validExitCodes        = @(0)
   url                   = $url
   url64                 = $url64
   checksum              = $checksum
@@ -20,4 +25,4 @@ $packageArgs = @{
   checksumType64        = $checksumType64
 }
 
-Get-ChocolateyWebFile @packageArgs
+Install-ChocolateyPackage @packageArgs
