8bc1cb3d848f21f7b1dedf6a8625f80f5dc6283c
Björn Strausmann <bjoern@strausmann.net>
2023-10-10 16:09:38 +0000

    Remove 64bit params

--- automatic/bambustudio/tools/chocolateyinstall.ps1
+++ automatic/bambustudio/tools/chocolateyinstall.ps1
@@ -6,9 +6,6 @@ $fileLocation = Join-Path $toolsDir 'BambuStudio.exe'
 $url                   = 'https://github.com/bambulab/BambuStudio/releases/download/v01.07.06.92/Bambu_Studio_win_public-v01.07.06.92-20230923002726.exe'
 $checksum              = '778ae3f81ab8c25effe7e168298ec4e62a0aca540917692dc753bee3318bc00b'
 $ChecksumType          = 'sha256'
-$url64                 = 'https://github.com/bambulab/BambuStudio/releases/download/v01.07.06.92/Bambu_Studio_win_public-v01.07.06.92-20230923002726.exe'
-$checksum64            = '778ae3f81ab8c25effe7e168298ec4e62a0aca540917692dc753bee3318bc00b'
-$ChecksumType64        = 'sha256'
 
 $packageArgs = @{
   packageName           = $env:ChocolateyPackageName
@@ -18,11 +15,8 @@ $packageArgs = @{
   silentArgs            = "/S"
   validExitCodes        = @(0)
   url                   = $url
-  url64                 = $url64
   checksum              = $checksum
-  checksum64            = $checksum64
   checksumType          = $checksumType
-  checksumType64        = $checksumType64
 }
 
 Install-ChocolateyPackage @packageArgs
