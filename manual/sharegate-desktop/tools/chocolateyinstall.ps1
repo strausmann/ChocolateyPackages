c7015752827161c9605acca9790bf01a25a09e59
Bjoern Strausmann <bjoern.strausmann@zea-ship.com>
2023-10-10 18:52:41 +0200

    ShareGate changed to manual process, appveyor blocked by CloudFlare.

--- manual/sharegate-desktop/tools/chocolateyinstall.ps1
+++ manual/sharegate-desktop/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = 'https://assets.sharegate.com/sharegate/downloads/ShareGate.20.0.2.msi'
-$checksum              = 'f2028928b14bf4d81efd7cf3b5cb1777a64a8903c4815a0853b80f49d6cf9f82'
+$url                   = 'https://assets.sharegate.com/sharegate/downloads/ShareGate.21.0.1.msi'
+$checksum              = '79045e4369aace4021e15bb5f97b91e19cf3a1cb5c55a5061d9691b995bb30ad'
 $checksumType          = 'sha256'
 
 $packageArgs = @{
@@ -14,11 +14,7 @@ $packageArgs = @{
   url           = $url
   checksum      = $checksum
   checksumType  = $checksumType
-  url64bit      = $url
-  checksum64    = $checksum
-  checksumType64= $checksumType
   destination   = $toolsDir
 }
 
-if (Get-Is32) { 'url64bit', 'checksum64', 'checksumType64' | ForEach-Object { $packageArgs.Remove($_) } }
 Install-ChocolateyPackage @packageArgs
