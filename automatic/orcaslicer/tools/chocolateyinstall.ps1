352e7578cd7712fc0a90175a5f2909aea3791431
Bjoern Strausmann <bjoern@strausmann.net>
2023-11-11 16:06:44 +0000

    AU: 1 updated - orcaslicer [skip ci] https://gist.github.com/strausmann/33cff89acf29181df2e5c7d2a3792d47/82d37d77cfc6d982c72465a124bc8c452e08e97e

--- automatic/orcaslicer/tools/chocolateyinstall.ps1
+++ automatic/orcaslicer/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $programs = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\"
 $shortcutFilePath = Join-Path $programs $linkName
 $targetPath   = Join-Path $toolsDir $fileName
 
-$url                   = 'https://github.com/SoftFever/OrcaSlicer/releases/download/v1.8.0-beta2/OrcaSlicer_Windows_V1.8.0-beta2_portable.zip'
-$checksum              = '34caa48360579a354a591adb6209c6528649e613188f1ae485a772193f69e034'
+$url                   = 'https://github.com/SoftFever/OrcaSlicer/releases/download/v1.8.0-rc/OrcaSlicer_Windows_V1.8.0-rc_portable.zip'
+$checksum              = 'f6d103395f4255732196a1d88080ff7f951dcf31e26a064be8cf51550620863b'
 $ChecksumType          = 'sha256'
 
 #Based on Custom
