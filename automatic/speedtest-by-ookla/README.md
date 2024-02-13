aee436b2354883d8b06f13dea5960788c06f65a9
Bjoern Strausmann <bjoern@strausmann.net>
2024-02-13 00:06:55 +0000

    AU: 1 updated - speedtest-by-ookla [skip ci] https://gist.github.com/strausmann/33cff89acf29181df2e5c7d2a3792d47/cec01d8617023bd0646f3db3e9691f8bedce3ba3

--- automatic/speedtest-by-ookla/README.md
+++ automatic/speedtest-by-ookla/README.md
@@ -1,4 +1,4 @@
-﻿# <img src="https://cdn.jsdelivr.net/gh/strausmann/ChocolateyPackages/icons/ookla.png" width="48" height="48"/> [speedtest-by-ookla](https://community.chocolatey.org/packages/speedtest-by-ookla)
+# <img src="https://rawcdn.githack.com/strausmann/ChocolateyPackages/17b3b7d6f418140a3828f9bedf56379cbe689827/icons/ookla.png" width="48" height="48"/> [speedtest-by-ookla](https://community.chocolatey.org/packages/speedtest-by-ookla)
 
 Running a speed test has never been so fast or easy. Download the free Speedtest desktop app for Windows and check your Internet speeds with the touch of a button. Get a real-time overview of your Internet service provider's performance and spot trends over time thanks to the following data:
 
--- automatic/speedtest-by-ookla/tools/chocolateyinstall.ps1
+++ automatic/speedtest-by-ookla/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference	= 'Stop';
 $toolsDir				= '$(Split-Path -parent $MyInvocation.MyCommand.Definition)'
 
-$url					= 'https://install.speedtest.net/app/windows/1.11.165/speedtestbyookla_x86.msi'
-$checksum				= 'f7c64456c2a6b9a934734e385758f83f70aac8552c2cbee70bf0f01abfddcee1'
+$url					= 'https://install.speedtest.net/app/windows/1.12.186/speedtestbyookla_x86.msi'
+$checksum				= '28e4e18f82a95bad8cd7547318618b25349443c7373804f54518788d967426c6'
 $checksumType           = 'sha256'
-$url64					= 'https://install.speedtest.net/app/windows/1.11.165/speedtestbyookla_x64.msi'
-$checksum64				= 'e12ccf3aa0f0a81870402d5dff6639eb2a7b30ffd367decd2b48ca1c80429f43'
+$url64					= 'https://install.speedtest.net/app/windows/1.12.186/speedtestbyookla_x64.msi'
+$checksum64				= '87e528a644575a4774111d5694dc50ac4962abf50ad7b26c585a14820090a181'
 $checksumType64         = 'sha256'
 
 #Based on Custom
