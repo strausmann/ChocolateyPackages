f49d65f3e7e7d683d3cc669359940ef2d74a59dc
Bjoern Strausmann <bjoern.strausmann@zea-ship.com>
2024-02-21 11:34:49 +0100

    Update PKT Package

--- automatic/pkt/update.ps1
+++ automatic/pkt/update.ps1
@@ -1,40 +1,13 @@
-import-module au
-. $PSScriptRoot\..\_scripts\all.ps1
+Import-Module AU
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
+Import-Module "../../scripts/au_extensions.psm1"
 
-$releases = ''
-
-function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
-            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
-            "(?i)(^\s*softwareName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)*'"
-            "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
-
-            '(?i)(^\s*file\s*=\s*)(".*")'   = "`$1`"`$toolsPath\$($Latest.FileName32)`""
-            '(?i)(^\s*file64\s*=\s*)(".*")' = "`$1`"`$toolsPath\$($Latest.FileName64)`""
-            }
-        }
-
-        "$($Latest.PackageName).nuspec" = @{
-            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
-        }
-
-        ".\legal\VERIFICATION.txt" = @{
-          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
-          "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
-          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
-          "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
-        }
-    }
-}
+$releases 		= 'https://pkt.cash/wallet/'
+$ChecksumType	= 'sha256'
 
 function global:au_BeforeUpdate {
-     Get-RemoteFiles -Purge -NoSuffix 
-
+    Get-RemoteFiles -Purge -NoSuffix 
     Set-Alias 7z $Env:chocolateyInstall\tools\7z.exe
     7z e tools\*.zip -otools *.exe -r -y
     rm tools\*.zip -ea 0
@@ -43,7 +16,7 @@ function global:au_BeforeUpdate {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $re      = '\.exe$'
+    $re      = '\PktWorldWalletSetup.exe$'
     $url     = $download_page.links | ? href -match $re | select -First 1 -expand href
     $domain  = $releases -split '(?<=//.+)/' | select -First 1
     $version = $url -split '[._-]|.exe' | select -Last 1 -Skip 2
@@ -56,4 +29,23 @@ function global:au_GetLatest {
     }
 }
 
-update -ChecksumFor none
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
+            "(?i)(^\s*softwareName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)*'"
+            "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
+        }
+
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+
+    }
+}
+
+update -ChecksumFor 32
