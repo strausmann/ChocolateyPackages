9f0fb37162a39f77125c159dceba51dbcd47303b
Björn Strausmann <bjoern@strausmann.net>
2024-06-09 16:50:34 +0200

    Update chocolateyinstall.ps1
    Fix BambuLab URL

--- automatic/bambustudio/tools/chocolateyinstall.ps1
+++ automatic/bambustudio/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'BambuStudio.exe'
 
-$url                   = 'https://github.com/bambulab/BambuStudio/releases/download/v01.09.01.66/Bambu_Studio_win_public-v01.09.01.66-20240511203248.exe'
-$checksum              = 'd51be9289cbdf080c47c28f6dbba4c736e162162bf5f26dd9ba13dd7e496eb3e'
+$url                   = 'https://github.com/bambulab/BambuStudio/releases/download/v01.09.01.66/Bambu_Studio_win_public-v01.09.01.67-20240516173854.exe'
+$checksum              = 'A1FA7C37328D3A92C297AE92DE9665033419D3646FEE15817A738B75B91A18F2'
 $ChecksumType          = 'sha256'
 
 $packageArgs = @{
