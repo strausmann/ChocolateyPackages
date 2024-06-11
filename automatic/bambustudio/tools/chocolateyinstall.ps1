da658d8a5a1574f131ca259ebcd152f3d1a51169
Bjoern Strausmann <bjoern.strausmann@zea-ship.com>
2024-06-11 14:30:34 +0200

    Manual Update Bambu Studio

--- automatic/bambustudio/tools/chocolateyinstall.ps1
+++ automatic/bambustudio/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'BambuStudio.exe'
 
-$url                   = 'https://github.com/bambulab/BambuStudio/releases/download/v01.09.01.66/Bambu_Studio_win_public-v01.09.01.67-20240516173854.exe'
-$checksum              = 'A1FA7C37328D3A92C297AE92DE9665033419D3646FEE15817A738B75B91A18F2'
+$url                   = 'https://github.com/bambulab/BambuStudio/releases/download/untagged-1a75cd1d9d18ef2af9d1/Bambu_Studio_win_public-v01.09.02.57-20240607194639.exe'
+$checksum              = 'FC0BC24352D08648636A0728BD98C550BB07175EB6817D595ACF51587351AF57'
 $ChecksumType          = 'sha256'
 
 $packageArgs = @{
