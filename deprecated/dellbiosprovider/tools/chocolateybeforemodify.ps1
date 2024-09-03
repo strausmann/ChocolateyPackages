59c212493d2b5988fae0a71ba1e8a893f98d1623
Bjoern Strausmann <bjoern.strausmann@zea-ship.com>
2024-09-03 14:20:42 +0200

    Remove old dellbiosprovider package

--- deprecated/dellbiosprovider/tools/chocolateybeforemodify.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'
-
-$moduleName = $env:ChocolateyPackageName      # this could be different from package name
-
-if (Get-Module -ListAvailable -Name $moduleName -ErrorAction SilentlyContinue){
-	Get-InstalledModule -Name $moduleName | Uninstall-Module -AllVersions -Force -ErrorAction SilentlyContinue -Verbose
-}
--- deprecated/dellbiosprovider/tools/chocolateyuninstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'
-$moduleName = $env:ChocolateyPackageName  # this may be different from the package name and different case
-
-Get-InstalledModule -Name $moduleName | Uninstall-Module -AllVersions -Force -ErrorAction SilentlyContinue
