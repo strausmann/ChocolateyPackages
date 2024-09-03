cccf5c93a206fec469524b821b26f4b8f7b8f4aa
Bjoern Strausmann <bjoern.strausmann@zea-ship.com>
2024-09-03 15:33:33 +0200

    fix microsoft-sql-server-migration-assistant-for-access search pattern

--- automatic/microsoft-sql-server-migration-assistant-for-access/tools/chocolateyinstall.ps1
+++ automatic/microsoft-sql-server-migration-assistant-for-access/tools/chocolateyinstall.ps1
@@ -2,21 +2,23 @@
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url                   = 'https://download.microsoft.com/download/D/B/D/DBDECC99-4DCA-4674-983A-CC1ABABA2B37/SSMAforAccess_9.0.0_x86.msi'
 $checksum              = '1bdae244909a47061bf39a616a6dbcc53eff338931c4a50367c748928eaae008'
-$urlx64                = 'https://download.microsoft.com/download/D/B/D/DBDECC99-4DCA-4674-983A-CC1ABABA2B37/SSMAforAccess_9.0.0.msi'
-$checksumx64           = '4b7c0c087c776b46a0d31584b27d19cc9777cb323a10ee23cfd6ac188e477e91'
+$checksumType          = 'sha256'
+$url64                 = 'https://download.microsoft.com/download/D/B/D/DBDECC99-4DCA-4674-983A-CC1ABABA2B37/SSMAforAccess_9.0.0.msi'
+$checksum64            = '4b7c0c087c776b46a0d31584b27d19cc9777cb323a10ee23cfd6ac188e477e91'
+$checksumType64        = 'sha256'
 
 $packageArgs = @{
 	packageName    = $env:ChocolateyPackageName
 	softwareName   = 'Microsoft SQL Server Migration Assistant for Access*'
 	installerType  = 'MSI'
 	url            = $url
-	url64bit       = $urlx64
+	url64bit       = $url64
 	silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" ALLUSERS=1 LICENSE_ACCEPTED=1"
 	validExitCodes = @(0,1641,3010)
 	checksum       = $checksum
-	checksumType   = 'sha256'
-	Checksum64     = $checksumx64
-	ChecksumType64 = 'sha256'
+	checksumType   = $checksumType
+	Checksum64     = $checksum64
+	ChecksumType64 = $checksumType64
 }
 
 Install-ChocolateyPackage @packageArgs
