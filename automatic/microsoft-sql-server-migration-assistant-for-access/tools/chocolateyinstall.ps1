15e4bc1e0cd601e4023d562b6b322095042da668
Bjoern Strausmann <bjoern@strausmann.net>
2024-09-03 13:35:13 +0000

    AU: 1 updated - microsoft-sql-server-migration-assistant-for-access [skip ci] https://gist.github.com/strausmann/33cff89acf29181df2e5c7d2a3792d47/a6f5d9ee5bfb54dd7310b269192a66d53817c3dc

--- automatic/microsoft-sql-server-migration-assistant-for-access/tools/chocolateyinstall.ps1
+++ automatic/microsoft-sql-server-migration-assistant-for-access/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url                   = 'https://download.microsoft.com/download/D/B/D/DBDECC99-4DCA-4674-983A-CC1ABABA2B37/SSMAforAccess_9.0.0_x86.msi'
-$checksum              = '1bdae244909a47061bf39a616a6dbcc53eff338931c4a50367c748928eaae008'
+$url                   = 'https://download.microsoft.com/download/D/B/D/DBDECC99-4DCA-4674-983A-CC1ABABA2B37/SSMAforAccess_9.5.0_x86.msi'
+$checksum              = '2d257459d1dca3503c790317fc4a178d530b5c4550dc44628d7c35ec7cd7acab'
 $checksumType          = 'sha256'
-$url64                 = 'https://download.microsoft.com/download/D/B/D/DBDECC99-4DCA-4674-983A-CC1ABABA2B37/SSMAforAccess_9.0.0.msi'
-$checksum64            = '4b7c0c087c776b46a0d31584b27d19cc9777cb323a10ee23cfd6ac188e477e91'
+$url64                 = 'https://download.microsoft.com/download/D/B/D/DBDECC99-4DCA-4674-983A-CC1ABABA2B37/SSMAforAccess_9.5.0.msi'
+$checksum64            = 'b97e956d33385e8e9fa520822578c24d1180297963990455b2e80d164f47a7be'
 $checksumType64        = 'sha256'
 
 $packageArgs = @{
