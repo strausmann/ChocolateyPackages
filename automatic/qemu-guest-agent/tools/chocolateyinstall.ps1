939e80822ac4db0a87a5a90480552fc3b63d5b8c
Bjoern Strausmann <bjoern@strausmann.net>
2024-11-18 16:06:56 +0000

    AU: 1 updated - qemu-guest-agent [skip ci] https://gist.github.com/strausmann/33cff89acf29181df2e5c7d2a3792d47/6a1b8650b2db92dfc0d5b9f47fe68c0747741eac

--- automatic/qemu-guest-agent/tools/chocolateyinstall.ps1
+++ automatic/qemu-guest-agent/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference	= 'Stop';
 $toolsDir				= '$(Split-Path -parent $MyInvocation.MyCommand.Definition)'
 
-$url					= 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.262-1/virtio-win-gt-x86.msi'
-$checksum				= '729264c966fe6e4f765f8cf5bdfa54756e433d2ec8520867b64231f3e44816a9'
+$url					= 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.266-1/virtio-win-gt-x86.msi'
+$checksum				= 'c40043157c1b403721554e1d9841215438404a6580d360a7b080b209e9f30cf0'
 $checksumType           = 'sha256'
-$url64					= 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.262-1/virtio-win-gt-x64.msi'
-$checksum64				= 'dd3a012849e5f692b45a5fc81d4875ce8994fff66aa14f72d1375c71aea26b17'
+$url64					= 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.266-1/virtio-win-gt-x64.msi'
+$checksum64				= '37b9ee622cff30ad6e58dea42fd15b3acfc05fbb4158cf58d3c792b98dd61272'
 $checksumType64         = 'sha256'
 
 $packageArgs = @{
