c8fe1ec75bf7256520088bb0892a51017eb6874f
Bjoern Strausmann <bjoern@strausmann.net>
2025-03-28 16:08:06 +0000

    AU: 1 updated - bambustudio [skip ci] https://gist.github.com/strausmann/33cff89acf29181df2e5c7d2a3792d47/66638a20f47670801289d8d9a1dcfbbd0ca0036f

--- automatic/bambustudio/tools/chocolateyinstall.ps1
+++ automatic/bambustudio/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url                   = 'https://github.com/bambulab/BambuStudio/releases/download/v02.00.00.95/Bambu_Studio_win_public-v02.00.00.95-20250325211800.exe'
-$checksum              = 'fd177a6017964ec9a308cb075147256a1d66fc03ebcffe74e26074b733e2f9b4'
+$url                   = 'https://github.com/bambulab/BambuStudio/releases/download/v02.00.01.50/Bambu_Studio_win_public-v02.00.01.50-20250328181949.exe'
+$checksum              = 'cae9c8e788e968df1d849d79017e9bd4971801d7826db2e09972a3859b573e61'
 $ChecksumType          = 'sha256'
 
 $packageArgs = @{
