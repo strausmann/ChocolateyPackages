a937bb10955b95b500996119bd8391585a87121e
Björn Strausmann <bjoern.strausmann@branchenversicherung.ch>
2025-10-28 09:25:22 +0100

    Add Software Package Bambu Suite v1.1.2.50

---
+++ manual/bambusuite/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$url                   = 'https://github.com/bambulab/BambuStudio/releases/download/v02.03.00.70/Bambu_Studio_win_public-v02.03.00.70-20251014214058.exe'
+$checksum              = '0b0e6092f25f0d875e554d0657d9a1b5c753ec53b3063dca71b9d192669fc3ee'
+$ChecksumType          = 'sha256'
+
+$packageArgs = @{
+  packageName           = $env:ChocolateyPackageName
+  softwareName          = 'BambuStudio*'
+  fileType              = 'exe'
+  silentArgs            = "/S"
+  validExitCodes        = @(0)
+  url                   = $url
+  checksum              = $checksum
+  checksumType          = $checksumType
+}
+
+Install-ChocolateyPackage @packageArgs
