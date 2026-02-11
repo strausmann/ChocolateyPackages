16a48ca370f857b66991e5c4c2acb9a61e1e8f6d
Bjoern Strausmann <bjoern@strausmann.net>
2026-02-11 08:07:06 +0000

    AU: 1 updated - bambuconnect [skip ci] https://gist.github.com/strausmann/33cff89acf29181df2e5c7d2a3792d47/45311f9760832ca07a4ddc1fd17bca17977bbb97

--- automatic/bambuconnect/tools/chocolateyinstall.ps1
+++ automatic/bambuconnect/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url64                 = 'https://public-cdn.bblmw.com/upgrade/bambu-connect/updates/versions/2.2.0-beta.1/bambu-connect-v2.2.0-beta.1-win32-x64.exe'
-$checksum64            = 'c1750e3f83ee1a11fa550d24a05b347ccf7577a66bf8293a18d0538d46317208'
+$url64                 = 'https://public-cdn.bblmw.com/upgrade/bambu-connect/updates/versions/2.2.1-beta.2/bambu-connect-v2.2.1-beta.2-win32-x64.exe'
+$checksum64            = '4a9c6127a9b3407a587cd180eb3e795b1c7f4a13bb587e326366b9e1d196c8fd'
 $checksumType64        = 'sha256'
 
 $packageArgs = @{
