4dd7400d1383694dcd56510a6776995fa4e86ecd
Bjoern Strausmann <bjoern@strausmann.net>
2026-04-15 08:09:23 +0000

    AU: 1 updated - bambustudio [skip ci] https://gist.github.com/strausmann/33cff89acf29181df2e5c7d2a3792d47/c7b0b438dda471392c1bd6d9a0678d3ac2b2b4fd

--- automatic/bambustudio/tools/chocolateyinstall.ps1
+++ automatic/bambustudio/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url                   = 'https://github.com/bambulab/BambuStudio/releases/download/v02.05.03.61/Bambu_Studio_win-v02.05.03.61-20260414213734.exe'
+$url                   = 'https://github.com/bambulab/BambuStudio/releases/download/v02.05.03.62/Bambu_Studio_win-v02.05.03.61-20260414213734.exe'
 $checksum              = '9fb80d2a9a3930a9218770d5e719b1360794d4b7270de39351a3a0fee2142e91'
 $ChecksumType          = 'sha256'
 
