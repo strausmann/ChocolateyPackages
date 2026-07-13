3c9f14f010ab7af4a5792a8cca64954cbb0d1abe
Björn Strausmann <bjoern@strausmann.net>
2026-07-14 00:28:21 +0200

    Add Pangolin CLI

---
+++ automatic/pangolin-cli/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url64      = 'https://github.com/fosrl/cli/releases/download/0.10.1/pangolin-cli_windows_installer.msi'
+$checksum64 = '7ef8fcd833632ba0aaf272af99dbd5dba8ee835dc02681ddf07f4459685f81b9'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'msi'
+  url64bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+  softwareName   = 'Pangolin CLI*'
+  silentArgs     = '/quiet'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
