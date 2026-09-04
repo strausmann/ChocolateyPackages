3e1a794f6a56f0a1e47af1b6485bc105ec6c1684
Bjoern Strausmann <bjoern@strausmann.net>
2026-09-04 00:09:18 +0000

    AU: 2 updated - infisical infisical-cli.portable [skip ci] https://gist.github.com/strausmann/33cff89acf29181df2e5c7d2a3792d47/8a0641a6d00f338eb6e15a47e0e58f540036c580

--- automatic/infisical-cli.portable/tools/chocolateyinstall.ps1
+++ automatic/infisical-cli.portable/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url64      = 'https://github.com/Infisical/cli/releases/download/v0.43.128/cli_0.43.128_windows_amd64.zip'
-$checksum64 = '4749e0ddbe5096d506bbf558f013136ad01e952c1d095d39299875921906440c'
+$url64      = 'https://github.com/Infisical/cli/releases/download/v0.43.129/cli_0.43.129_windows_amd64.zip'
+$checksum64 = '6a875f60ba612509224ff04495a109f157757ba2f42bcd6f7479a108429d5b2c'
 
 # Upstream liefert fuer Windows ein Portable-Binary im Zip (infisical.exe) - keinen nativen Installer.
 # Install-ChocolateyZipPackage entpackt nach $toolsDir; Chocolatey legt automatisch einen 'infisical'-Shim
